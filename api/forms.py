51dabdc52397eac500fc36bd582945fc99d190f9
axtaylor <208505619+axtaylor@users.noreply.github.com>
2026-02-03 15:18:35 -0500

    feat: add usage hints for user registration form

--- api/forms.py
+++ api/forms.py
@@ -49,5 +49,5 @@ class TrainingLogForm(ModelForm):
 class UserInformationForm(ModelForm):
     class Meta:
         model = models.UserInformation
-        fields = ['height', 'age', 'units', 'gender']
+        fields = ['units', 'height', 'age', 'gender']
 
