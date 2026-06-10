a8442e3570e26814e9dbe8f9804acf4cb2e7a103
axtaylor <208505619+axtaylor@users.noreply.github.com>
2026-06-10 16:41:44 -0400

    add: rate limiting on user login with django axes

--- api/pages/_login_page.py
+++ api/pages/_login_page.py
@@ -2,11 +2,19 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from axes.decorators import axes_dispatch
+from axes.helpers import get_lockout_response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 
 '''
 Login Page for Registered Users
 
 '''
+
+@axes_dispatch
+@never_cache
 def login_page(request):
 
     if request.user.is_authenticated:
@@ -39,4 +47,19 @@ def login_page(request):
             'data': True,
             'form': form
         },
+    )
+
+def lockout_response(request, credentials, *args, **kwargs):
+    form = AuthenticationForm()
+    return render(
+        request,
+        'api/login_register.html',
+        {
+            'submit_type': 'login',
+            'data': True,
+            'form': form,
+            'locked_out': True,
+            'lockout_message': 'Too many failed attempts. Please try again in 3 minutes.',
+        },
+        status=200,
     )
